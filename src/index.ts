80e7d2198eb19fac9e81712b003d67046173d262
Ale Figueroa <afigueroa@goamv.com>
2019-02-10 13:22:24 -0600

    Fix default index

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { RequestHasher, YakTimeOpts, YakTimeServer } from './util'
+export { yaktime } from './yaktime'
+export { AsyncServer, createAsyncServer } from './AsyncServer'
+export { createYaktimeServer } from './yaktimeServer'
